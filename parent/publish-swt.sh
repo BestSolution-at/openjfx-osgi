fa46e4ebbd2ea308d841491686ed777a538fa5b4
Tom Schindl <tom.schindl@bestsolution.at>
2022-04-01 10:54:34 +0200

    update to 18

--- parent/publish-swt.sh
+++ parent/publish-swt.sh
@@ -1,7 +1,7 @@
 mvn deploy:deploy-file -DgroupId=at.bestsolution.openjfx \
   -DartifactId=javafx-swt \
-  -Dversion=16 \
+  -Dversion=18 \
   -Dpackaging=jar \
-  -Dfile=/Users/tomschindl/Downloads/javafx-sdk-16/lib/javafx-swt.jar \
-  -DrepositoryId=efxclipse-releases \
-  -Durl=http://rattenschwanz.bestsolution.at:8081/nexus/content/repositories/efxclipse-releases
+  -Dfile=/Users/tomschindl/Downloads/javafx-sdk-18/lib/javafx-swt.jar \
+  -DrepositoryId=oss-releases \
+  -Durl=https://maven.bestsolution.at/deploy/releases/
